8202d2f29148b58ed0822181f27b5d91fdde8ac9
Anonjsdev <152361885+Anonjsdev@users.noreply.github.com>
2023-12-01 18:45:12 +0800

    Add files via upload

--- src/components/aggregation/choices/SwapButton.tsx
+++ src/components/aggregation/choices/SwapButton.tsx
@@ -22,7 +22,7 @@ const SwapButton = () => {
   }
 
   const buttonBaseClasses =
-    "border rounded-full p-8 py-[16px] text-center text-[16px] text-white font-medium  font-general-sans border border-[#00d084]";
+    "border rounded-full p-8 py-[16px] text-center text-[16px] text-white font-medium  font-general-sans border border-[#1fc7d4]";
 
   const [buttonStatus, setButtonStatus] = useState<ButtonStatus>(
     ButtonStatus.ALL_OK
